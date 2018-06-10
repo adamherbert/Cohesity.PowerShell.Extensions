5572f43e8f8b1b587a476849dd1cea6bc10c8a51
Adam Herbert <adam.h.herbert@gmail.com>
2018-06-10 16:23:26 -0400

    Finished what should be a functioning proof of concept of an API call

--- src/Private/Invoke-CohesityAPI.ps1
+++ src/Private/Invoke-CohesityAPI.ps1
@@ -2,27 +2,88 @@ function Invoke-CohesityAPI {
   [CmdletBinding()]
   param (
     # Method
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [ValidateSet('get', 'post', 'put', 'delete')]
     [String]
-    $Method,
+    $RequestMethod,
     # URI
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [String]
-    $URI
+    $RequestTarget,
+    # Data Payload
+    [Parameter(Mandatory = $false)]
+    [hashtable]
+    $RequestArguments,
+    # Request Headers
+    [Parameter(Mandatory = $false)]
+    [hashtable]$RequestHeaders = @{}
   )
   
   begin {
+    # Set minimum required headers
+    $RequestHeaders['accept'] = 'application/json'
+    $RequestHeaders['content-type'] = 'application/json'
+
+    # Validate that we have a target VIP
+    if ([string]::IsNullOrEmpty($script:CohesityVIP)) {
+      Write-Error 'Please provide Cohesity VIP before API calls!'
+    }
+        
+    # Validate that we are logged in or that a login call is being made
     if ($Method -ieq 'post' -and $URI -match 'accessTokens') {
       continue
     }
-    elseif (-not $script:CohesityToken) {
+    elseif ([string]::IsNullOrEmpty($script:CohesityToken)) {
       Write-Error 'Please authenticate before making any Cohesity API calls!'
     }
   }
   
   process {
+    # Create full URI based on RequestTarget
+    $uri = "https://$($script:CohesityVIP)/irisservices/api/v1"
+    if ($RequestTarget -notmatch "/") {
+      $RequestTarget = "/public/$RequestTarget"
+    }
+    [string]$uri = (New-Object -TypeName 'System.Uri' -ArgumentList ([System.Uri]$uri),$RequestTarget).AbsoluteUri
 
+    # If RequestMethod is GET then put parameters on URI
+    if ( $Method -ieq 'get' ) {
+      if ($RequestArguments.Count -gt 0) {
+        $uri += '?'
+        $uri += [string]::join("&", @(
+            foreach ($pair in $RequestArguments.GetEnumerator()) { 
+              if ($pair.Name) { 
+                $pair.Name + '=' + $pair.Value 
+              } 
+            }))
+      }
+      try {
+        $result = Invoke-RestMethod `
+          -SkipCertificateCheck:$true `
+          -ContentType 'application/json' `
+          -Headers $RequestHeaders `
+          -Uri $uri
+      }
+      catch {
+        Write-Error $_.Exception.Message
+      }
+    }
+    # All other request methods will send a JSON payload
+    else {
+      try {
+        $result = Invoke-RestMethod `
+          -SkipCertificateCheck:$true `
+          -ContentType 'application/json' `
+          -Headers $RequestHeaders `
+          -Uri $uri `
+          -Body ($RequestArguments | ConvertTo-Json -Depth 100)
+      }
+      catch {
+        Write-Error $_.Exception.Message
+      }
+    }
+
+    Write-Output $result | Format-List
   }
   
   end {
---
+++ src/Public/Connect-Cohesity.ps1
@@ -0,0 +1,40 @@
+function Connect-Cohesity {
+  [CmdletBinding()]
+  param (
+    # Cohesity Virtual IP (VIP)
+    [Parameter(Mandatory=$true)]
+    [string]
+    $CohesityVIP,
+    # Credentials
+    [Parameter(Mandatory=$false)]
+    [pscredential]
+    $Credential
+  )
+  
+  begin {
+    if (-not $Credential) {
+      $Credential = Get-Credential `
+        -Message "Please provide username and password for Cohesity VIP ($CohesityVIP)" `
+        -Title "Cohesity Login to $CohesityVIP" 
+    }
+  }
+  
+  process {
+    $networkCredential = $Credential.GetNetworkCredential()
+
+    $RequestArguemnts = @{
+      'domain' = $networkCredential.Domain
+      'username' = $networkCredential.UserName
+      'password' = $networkCredential.Password
+    }
+
+    Invoke-CohesityAPI `
+      -RequestTarget 'accessTokens' `
+      -RequestMethod 'POST' `
+      -RequestArguments $RequestArguemnts
+      -ErrorAction 'Stop'
+  }
+  
+  end {
+  }
+}
